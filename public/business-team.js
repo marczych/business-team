84cb5446265bc89feae79f1d8692b63b89e28a3f
Taylor Arnicar <MutantFrk@gmail.com>
2015-01-24 08:16:39 -0800

    Try defining JS progress bar class a different way
    Also try writing functions a different way. Minimal progress towards
    actually animating it.

--- public/business-team.js
+++ public/business-team.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
+
+   var myBar = new progressBar(50);
+
+   // Animate the progress bar
+      animate(myBar);
+      console.log("finished first animation");
+
 	//newPanel();
 });
 
@@ -11,13 +18,37 @@ function newPanel(name, action, size) {
 function wipeRow() {
 	$('#topRow').empty();
 }
+function animate(myBar) {
+   console.log("starting to animate");
+   myBar.adjustProgressBar();
+   setTimeout(myBar.animate, 100);
+}
 
-var progressBar = function (startingPercent = 0) {
+function progressBar(startingPercent) {
    this.percent = startingPercent;
-	$('#progressbar').append("<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: " + this.percent + "%">");
+   $('#progressbar').append("<div class='progress-bar progress-bar-striped active' id='thebaritself' role='progressbar' aria-valuenow='45' aria-valuemin='0' aria-valuemax='100' style='width: " + this.percent + "%'>");
 
+   this.getPercent = function() {
+      return this.percent;
+   }
+
+   this.setPercent = function(newPercent) {
+      $("#thebaritself").width(newPercent);
+      this.percent = newPercent;
+   }
+
+   this.adjustProgressBar = function() {
+      console.log("starting to adjust");
+      console.log("percent is " + this.percent);
+
+      if (this.percent >= 100) {
+         console.log("got too wide");
+         this.setPercent(0);
+      }
+
+      this.setPercent(this.getPercent() +10);
+      console.log("percent is now " + this.percent);
+
+   }
 }
 
-progressBar.prototype.setPercent = function(newPercent) {
-   this.percent = newPercent;
-}
