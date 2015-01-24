c642f0f50c65b8520e5862303fc2c8a4796cdf5d
Andrew Goldberg <andrew@ifixit.com>
2015-01-24 08:35:50 -0800

    Add a rad panel class and an array thereof.
    Taylor taylor taylor tayloR.

--- public/business-team.js
+++ public/business-team.js
@@ -6,13 +6,13 @@ $( document ).ready(function() {
       animate(myBar);
       console.log("finished first animation");
 
-	//newPanel();
+	panels[0].makePanel();
 });
 
 //$('#pane').append("<div class='col-md-5'>content</div>");
 
-function newPanel(name, action, size) {
-	$('#topRow').append("<div class='col-md-" + size + "'>" + name + "<br /><button type='button' class='btn btn-default btn-primary'>" + action + "</button></div>");
+function newPanel(control, action, size) {
+	$('#topRow').append("<div class='col-md-" + size + "'>" + control + "<br /><button type='button' class='btn btn-default btn-primary'>" + action + "</button></div>");
 }
 
 function wipeRow() {
@@ -52,3 +52,28 @@ function progressBar(startingPercent) {
    }
 }
 
+panels = new Array();
+panels.push(new Panel("Business Socks", "switch", "3", "Put on", "Take off"))
+
+function Panel(control, inputType, divWidth, action1, action2, action3) {
+	this.control = control;
+	this.action1 = action1;
+	this.action2 = action2;
+	this.action3 = action3;
+	this.inputType = inputType;
+	this.divWidth = divWidth;
+
+	this.makePanel = function() {
+		console.log(inputType);
+		switch (this.inputType) {
+			case "button":
+				$('#topRow').append("<div class='col-md-" + divWidth + "'>" + control + "<br /><button type='button' class='btn btn-default btn-primary'>" + action1 + "</button></div>");
+				break;
+
+			case "switch":
+				console.log("yolo");
+				$('#topRow').append("<div class='col-md-" + divWidth + "'>" + control + "<br /><div class='btn-group' data-toggle='buttons'><label class='btn btn-primary'><input type='radio' name='control' id='option1' autocomplete='off' checked>" + action1 + "</label><label class='btn btn-primary'><input type='radio' name='options' id='option2' autocomplete='off'>" + action2 + "</label></div></div>");
+				break;
+		}
+	}
+}
