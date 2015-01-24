5f8d3d4f457a1f4580af068ba09289a0a97687a7
Taylor Arnicar <MutantFrk@gmail.com>
2015-01-24 06:36:14 -0800

    Make the beginnings of a Progress Bar class
    I'm sorry if everything is wrong. I've had no sleep, and I don't know
    how to JavaScript yet.

--- public/business-team.js
+++ public/business-team.js
@@ -11,3 +11,13 @@ function newPanel(name, action, size) {
 function wipeRow() {
 	$('#topRow').empty();
 }
+
+var progressBar = function (startingPercent = 0) {
+   this.percent = startingPercent;
+	$('#progressbar').append("<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: " + this.percent + "%">");
+
+}
+
+progressBar.prototype.setPercent = function(newPercent) {
+   this.percent = newPercent;
+}
