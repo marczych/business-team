cd2646d69eeafcad492dd1d1a1c6277a60c9232a
Christopher Patton <chpatton013@gmail.com>
2015-01-24 15:11:43 -0800

    What, like he's people? Mother! He thinks he's people!

--- index.js
+++ index.js
@@ -12,24 +12,5 @@ server.listen(port, function () {
 // Routing
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (socket) {
-   socket.on('keypress', function(data) {
-      console.log('keypress', data.keyCode);
-
-      // Send keypresses to all clients.
-      socket.emit('received keypress', data);
-      socket.broadcast.emit('received keypress', data);
-   });
-
-   socket.on('disconnect', function () {
-      console.log('disconnect');
-   });
-});
-
-// The server can send arbitrary events at arbitrary times.
-(function loop() {
-   // Send to all clients.
-   io.emit('loop', 'LOOP');
-
-   setTimeout(loop, 1000);
-})();
+var gameServer = require('./game-server')(io);
+gameServer.play();
--- public/main.js
+++ public/main.js
@@ -1,18 +1,137 @@
 $(function() {
-  var $window = $(window);
-  var socket = io();
+   var $window = $(window);
+   var socket = io();
+   var identifier = undefined;
+   var states = {
+      lobby: {},
+      game: {},
+      results: {},
+   };
+   var state;
 
-  $window.keydown(function (event) {
-     socket.emit('keypress', {
-        keyCode: event.keyCode
-     });
-  });
+   $(document).ready(function() {
+      state = states.lobby;
+   });
 
-  socket.on('received keypress', function (data) {
-     console.log(data.keyCode);
-  });
+   $window.keydown(function(ev) {
+      socket.emit('keypress', {
+         keyCode: ev.keyCode
+      });
+   });
 
-  socket.on('loop', function (data) {
-     console.log('loop', data);
-  });
+   socket.on('connected', function(data) {
+      console.log('connected');
+
+      if (state != states.lobby) {
+         console.error('received connected event when not in lobby state');
+         return;
+      }
+
+      identifier = data;
+      console.log(data);
+   });
+
+   socket.on('game in progress', function() {
+      console.error('cannot join game in progress');
+   });
+
+   socket.on('start game', function() {
+      console.log('start game');
+
+      if (state != states.lobby) {
+         console.error('received start game event when not in lobby state');
+         return;
+      }
+
+      if (!identifier) {
+         console.error('received start game event before identifier');
+      }
+
+      state = states.game;
+
+      // load game things
+      socket.emit('game loaded', identifier);
+   });
+
+   socket.on('start stage', function() {
+      console.log('stage update');
+
+      if (state != states.state) {
+         console.error('received start stage event when not in game state');
+         return;
+      }
+
+      if (!identifier) {
+         console.error('received start stage event before identifier');
+      }
+
+      // load stage things
+      socket.emit('stage loaded', identifier);
+   });
+
+   socket.on('state update', function() {
+      console.log('state update');
+
+      if (state != states.state) {
+         console.error('received state update event when not in game state');
+         return;
+      }
+
+      if (!identifier) {
+         console.error('received state update event before identifier');
+      }
+
+      // update state
+   });
+
+   $window.bind('lobby ready', function(ev) {
+      console.log('lobby ready');
+
+      if (state != states.lobby) {
+         console.error('fired lobby ready event when not in lobby state');
+         return;
+      }
+
+      if (!identifier) {
+         console.error('fired lobby ready event before identifier');
+         return;
+      }
+
+      socket.emit('lobby ready', identifier);
+   });
+
+   $window.bind('lobby not ready', function(ev) {
+      console.log('lobby not ready');
+
+      if (state != states.lobby) {
+         console.error('fired lobby not ready event when not in lobby state');
+         return;
+      }
+
+      if (!identifier) {
+         console.error('fired lobby not ready event before identifier');
+         return;
+      }
+
+      socket.emit('lobby not ready', identifier);
+   });
+
+   $window.bind('action taken', function(ev) {
+      console.log('action taken');
+
+      if (state != states.game) {
+         console.error('fired action taken event when not in game state');
+         return;
+      }
+
+      if (!identifier) {
+         console.error('fired action taken event before identifier');
+         return;
+      }
+
+      socket.emit('action taken', {
+         identifier: identifier,
+         action: ev,
+      });
+   });
 });
