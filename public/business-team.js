ea7ccece8886e8c8645ebdf5d4c7de7a9784a2de
Andrew Goldberg <andrew@Andrews-MacBook-Pro.local>
2015-01-24 05:51:05 -0800

    Add buttons to HTML to call JS function to create a div
    taylor.

---
+++ public/business-team.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+	//newPanel();
+});
+
+//$('#pane').append("<div class='col-md-5'>content</div>");
+
+function newPanel(name, action, size) {
+	$('#topRow').append("<div class='col-md-" + size + "'>" + name + "<br /><button type='button' class='btn btn-default btn-primary'>" + action + "</button></div>");
+}
