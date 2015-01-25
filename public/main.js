819a075f18d7ee603186a0b0614a3b936fdcaa55
Marc Zych <marczych@gmail.com>
2015-01-24 21:31:58 -0800

    Change main app file

--- public/main.js
+++ public/main.js
@@ -1,13 +1,23 @@
+requirejs.config({
+   "paths": {
+      "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
+      "jquery-ui": "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min",
+      "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min"
+   }
+});
+
 define([
    'jquery',
    'jquery-ui',
    'business-team',
-   'socket.io/socket.io'
+   'socket.io/socket.io',
+   'business-team'
 ], function(
    jquery,
    jqueryui,
    bt,
-   io
+   io,
+   bt
 ) {
    var $window = $(window);
    var socket = io();
